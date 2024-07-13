27540391c8f645f088790a292dce2b2fd94c85c8
pzprado <pete.zprado@gmail.com>
2024-07-13 16:21:04 +0200

    test: working createAgreement

--- test/CreateContractTest.t.sol
+++ test/CreateContractTest.t.sol
@@ -15,5 +15,23 @@ contract CreateContractTest is Test {
         shotgun = new Shotgun();
     }
 
-    function testCreateAgreement() public {}
+    function testCreateAgreement() public {
+        uint256 initialAgreementCounter = shotgun.agreementCounter();
+
+        // Create an agreement
+        uint256 agreementId = shotgun.createAgreement(party1, party2, token, duration);
+
+        // Validate that the agreement counter has increased
+        uint256 newAgreementCounter = shotgun.agreementCounter();
+        assertEq(newAgreementCounter, initialAgreementCounter + 1);
+
+        // Validate the agreement details
+        (address _party1, address _party2, address _token, uint256 _duration,,,,) = shotgun.agreements(agreementId);
+
+        assertEq(_party1, party1);
+        assertEq(_party2, party2);
+        assertEq(_token, token);
+        assertEq(_duration, duration);
+        console.log("duration: %s", duration);
+    }
 }
