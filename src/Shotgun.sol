08f2790e8d0981812cf094c03556cb26ca1fba8b
pzprado <pete.zprado@gmail.com>
2024-07-13 02:24:11 +0200

    feat: make offer clause

--- src/Shotgun.sol
+++ src/Shotgun.sol
@@ -5,25 +5,36 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Shotgun is Ownable {
-    IERC20 public shareToken;
-    uint256 public offerPrice;
-    address public offeror;
-    uint256 public offerExpiry;
-    bool public offerAccepted;
-    uint256 public sharesAmount;
-
-    event OfferMade(address indexed offeror, uint256 price, uint256 sharesAmount, uint256 expiry);
-    event OfferAccepted(address indexed acceptor);
-    event OfferCountered(address indexed counterOfferor);
-    event OfferRejected(address indexed rejector);
-
-    constructor(address _shareToken) {
-        shareToken = IERC20(_shareToken);
+    struct Offer {
+        address token;
+        uint256 price;
+        uint256 tokenAmount;
+        address offeror;
+        uint256 expiry;
+        bool active;
     }
 
-    function makeOffer(uint256 price, uint256 shares, uint256 duration) external {
-        // TODO: add logic
-        emit OfferMade(msg.sender, price, shares, offerExpiry);
+    Offer public currentOffer;
+
+    event OfferMade(address indexed offeror, address token, uint256 price, uint256 shares, uint256 expiry);
+    event OfferAccepted(address indexed offeree);
+    event CounterOfferMade(address indexed counterOfferor);
+    event OfferExpired();
+
+    function makeOffer(address token, uint256 price, uint256 shares, uint256 duration) external {
+        require(!currentOffer.active, "An offer is already active.");
+        require(IERC20(token).balanceOf(msg.sender) >= shares, "Insufficient shares to make offer.");
+
+        currentOffer = Offer({
+            token: token,
+            price: price,
+            shares: shares,
+            offeror: msg.sender,
+            expiry: block.timestamp + duration,
+            active: true
+        });
+
+        emit OfferMade(msg.sender, token, price, shares, currentOffer.expiry);
     }
 
     function acceptOffer() external {
